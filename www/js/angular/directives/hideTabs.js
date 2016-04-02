1e3d1cd90d1beb21c8a7bb57b3a76ffd07ab210f
Vidur <vidur.jain@livspace.com>
2016-04-02 14:54:04 +0530

    adding hide tabs support in a particular view

---
+++ www/js/angular/directives/hideTabs.js
@@ -0,0 +1,12 @@
+angular.module('app')
+.directive('hideTabs', function($rootScope) {
+  return {
+      restrict: 'A',
+      link: function($scope, $el) {
+          $rootScope.hideTabs = 'tabs-item-hide';
+          $scope.$on('$destroy', function() {
+              $rootScope.hideTabs = '';
+          });
+      }
+  };
+});
